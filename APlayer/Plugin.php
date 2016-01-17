3c24f7c38f1834d8c6755a3c1144475afcad0484
zgq354 <zgq1295@gmail.com>
2016-01-17 23:12:48 +0800

    support playlist change the shortcode

--- APlayer/Plugin.php
+++ APlayer/Plugin.php
@@ -1,11 +1,12 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
 /**
  * 炒鸡漂亮的html5播放器APlayer插件，支持在文章中插入html5播放器
  * 
  * @package APlayer
  * @author ZGQ
- * @version 1.0.1
+ * @version 1.1.0
  * @dependence 13.12.12-*
  * @link https://github.com/zgq354/APlayer-Typecho-Plugin
  */
@@ -130,17 +131,17 @@ class APlayer_Plugin implements Typecho_Plugin_Interface
 <script type="text/javascript" src="{$playerurl}APlayer.min.js"></script>
 <script>
 var len = aPlayerOptions.length;
-for(var i=0;i<len;i++){
-	aPlayers[i] = new APlayer({
-		element: document.getElementById('player' + aPlayerOptions[i]['id']),
+for(var ii=0;ii<len;ii++){
+	aPlayers[ii] = new APlayer({
+		element: document.getElementById('player' + aPlayerOptions[ii]['id']),
             narrow: false,
-            autoplay: aPlayerOptions[i]['autoplay'],
-            showlrc: aPlayerOptions[i]['showlrc'],
-            music: aPlayerOptions[i]['music']
+            autoplay: aPlayerOptions[ii]['autoplay'],
+            showlrc: aPlayerOptions[ii]['showlrc'],
+            music: aPlayerOptions[ii]['music'],
+            theme: aPlayerOptions[ii]['theme']
 	        });
-	aPlayers[i].init();
+	aPlayers[ii].init();
 }
-
 </script>
 EOF;
 	 }
@@ -155,9 +156,9 @@ EOF;
 	{
 		//屏蔽自动链接
 		if ($value['isMarkdown']) {
-			$value['text'] = preg_replace('/(?!<div>)\[(mp3)](.*?)\[\/\\1](?!<\/div>)/is','<div>[mp3]\\2[/mp3]</div>',$value['text']);
+			//$value['text'] = preg_replace('/(?!<div>)\[(mp3)](.*?)\[\/\\1](?!<\/div>)/is','<div>[mp3]\\2[/mp3]</div>',$value['text']);
 			//兼容JWPlayer
-			$value['text'] = preg_replace('/(?!<div>)<(jw)>(.*?)<\/\\1>(?!<\/div>)/is','<div><jw>\\2</jw></div>',$value['text']);
+			//$value['text'] = preg_replace('/(?!<div>)<(jw)>(.*?)<\/\\1>(?!<\/div>)/is','<div><jw>\\2</jw></div>',$value['text']);
 		}
 		return $value;
 	}
@@ -173,37 +174,179 @@ EOF;
 		$content = empty($lastResult) ? $content : $lastResult;
 
 		if ($widget instanceof Widget_Archive) {
-			$content = preg_replace_callback('/\[(mp3)](.*?)\[\/\\1]/si',array('APlayer_Plugin','parseCallback'),$content);
+			//解析列表播放器
+			$content = preg_replace_callback('/\[(music)(.*?)](.*?)\[\/\\1]/si',array('APlayer_Plugin','parselistCallback'),$content);
+			//解析单曲播放器
+			$content = preg_replace_callback('/\[(mp3)(.*?)](.*?)\[\/\\1]/si',array('APlayer_Plugin','parseCallback'),$content);
 		}
 
 		return $content;
 	}
+	
+	/**
+	 * 解析列表播放器
+	 * @param unknown $matches
+	 * @return string
+	 */
+	public static function parselistCallback($matches)
+	{
+		//播放器id
+		$id = self::getUniqueId();
+		
+		if (preg_match_all('/\[(mp3)(.*?)](.*?)\[\/\\1]/si', $matches[3] , $all)){
+			$result = array();
+			foreach ($all[3] as $k=>$v){
+				//获取所有music信息
+				$result[$k] = self::parse(trim($all[3][$k]),trim($all[2][$k]));
+			}
+		}
+		
+		$data = array(
+			'id' => $id ,
+			'autoplay' => false,
+			'showlrc' => false,
+			'theme' => '#e6d0b2'
+		);
+		
+		//默认有歌词就显示
+		foreach ($result as $v){
+			if ($v['lyric']) $data['showlrc'] = true;
+		}
+		
+		//获取播放器属性
+		$atts = explode('|',trim($matches[2]));
+		foreach ($atts as $att) {
+			if (empty($att)) continue;
+			$pair = explode('=',$att);
+			$data[trim($pair[0])] = trim($pair[1]);
+		}
+		
+		
+		//自动播放
+		$data['autoplay'] = boolval($data['autoplay']) && $data['autoplay'] != 'false';
+		//歌词
+		$data['showlrc'] = boolval($data['showlrc']) && $data['showlrc'] != 'false';
+
+		//输出代码，先输出歌词
+		$playerCode =  '<div id="player'.$id.'" class="aplayer">
+		';
+		
+		$lrcCode = '';
+		foreach ($result as $k=>$v){
+			//歌词不存在的时候输出
+			$lrc = $v['lyric'] ? $v['lyric'] : '[00:00.00]Lyric not found...';
+			$lrcCode .= '<pre class="aplayer-lrc-content">'."\n".$lrc."\n</pre>\n";
+			
+			//清理多余参数, 确保lrc内容不输出到json里面
+			unset($result[$k]['lrc']);
+			unset($result[$k]['cover']);
+			unset($result[$k]['lyric']);
+			unset($result[$k]['artist']);
+		}
+		
+		if ($data['showlrc']) {
+			$playerCode .= $lrcCode;
+		}
+		$playerCode .= "</div>\n";
+		
+		//开始添加歌曲列表
+		$data['music'] = $result;
+		
+		//加入头部数组
+		$js = json_encode($data);
+		$playerCode .= <<<EOF
+		<script>aPlayerOptions.push({$js});</script>
+EOF;
+		
+		return $playerCode;
+	}
 
 	/**
-	 * 参数回调解析，自动下载歌词
+	 * 解析单曲播放器
 	 * 
 	 * @param array $matches
 	 * @return string
 	 */
 	public static function parseCallback($matches)
-	{
-		$all = $matches[2];
-		$lyric = false;
-		if( preg_match('/\[(lrc)](.*?)\[\/\\1]/si', $all ,$lyrics) ){
-			$lyric = $lyrics[2];
+	{		
+		$data = self::parse($matches[3],$matches[2]);
+		
+		//播放器id
+		$id = self::getUniqueId();
+		
+		//参数设置
+		$options = array(
+			'id' => $id ,
+			'autoplay' => false,
+			'showlrc' => false,
+			'theme' => '#e6d0b2'
+			);
+		
+		//判断是否有歌词
+		if ($data['lyric']) {
+			$options['showlrc'] = true;
 		}
 
-		$all = preg_replace('/\[(lrc)](.*?)\[\/\\1]/si', '', $all);
+		if (isset($data['autoplay'])) {
+			$options['autoplay'] = boolval($data['autoplay']) && $data['autoplay'] != 'false';
+		}
+		
+		//主题颜色
+		$options['theme'] = isset($data['theme'])?$data['theme']:$options['theme'];
+		
+		//添加音乐
+		$options['music']['title'] = $data['title'];
+		$options['music']['author'] = $data['author'];
+		$options['music']['url'] = isset($data['url']) ? $data['url'] : '';
+		$options['music']['pic'] = isset($data['pic']) ? $data['pic'] : '';
+		
+		//输出代码
+		$playerCode =  '<div id="player'.$id.'" class="aplayer">
+		';
+		if ($data['lyric']) {
+			$playerCode .= '<pre class="aplayer-lrc-content">'."\n".$data['lyric']."\n</pre>\n";
+		}
+		$playerCode .= "</div>\n";
+		//加入头部数组
+		$js = json_encode($options);
+		$playerCode .= <<<EOF
+		<script>aPlayerOptions.push({$js});</script>
+EOF;
+		
+		return $playerCode;
+	}
 
-		$atts = explode('|',$all);
+	/**
+	 * 获取一个唯一的id
+	 * @return number
+	 */
+	public static function getUniqueId()
+	{
+		self::$playerID++;
+		return self::$playerID;
+	}
+	
+	/**
+	 * 解析一首歌
+	 * 
+	 * @param string $line [mp3]标签内的内容
+	 * @return multitype: data
+	 */
+	private static function parse($content = '',$attr = '')
+	{
+		$lyric = false;
+		if( preg_match('/\[(lrc)](.*?)\[\/\\1]/si', $content ,$lyrics) ){
+			$lyric = $lyrics[2];
+		}
+		
+		$data = array();
 		
 		//mp3链接
-		$files = array_shift($atts);
+		$data['url'] = preg_replace('/\[(lrc)](.*?)\[\/\\1]/si', '', $content);;
 		
-		//其他参数
-		$data = array();
-
+		$atts = explode('|',trim($attr));
 		foreach ($atts as $att) {
+			if (empty($att)) continue;
 			$pair = explode('=',$att);
 			$data[trim($pair[0])] = trim($pair[1]);
 		}
@@ -215,6 +358,7 @@ EOF;
 		}
 
 		$data['lyric'] = $lyric;
+		
 
 		//解析封面
 		if(!isset($data['cover'])){
@@ -231,64 +375,18 @@ EOF;
 				}
 			}
 		}
-
-		return self::getPlayer($files,$data);
-	}
-
-	/**
-	 * 输出播放器实例
-	 * 
-	 * @param string $source
-	 * @param array $playerOptions
-	 * @return string
-	 */
-	public static function getPlayer($source,$playerOptions = array())
-	{
-		//播放器id
-		$id = self::$playerID;
-		self::$playerID++;
-		//参数设置
-		$options = array(
-			'id' => $id ,
-			'autoplay' => false,
-			'showlrc' => false,
-			);
-		//判断是否有歌词
-		if ($playerOptions['lyric']) {
-			$options['showlrc'] = true;
-		}
-
-		if (isset($playerOptions['autoplay'])) {
-			if ($playerOptions['autoplay']) {
-				$options['autoplay'] = true;
-			}
-		}
 		
+		$data['author'] = isset($data['artist']) ? $data['artist']:'Unknown';
+		$data['title'] = isset($data['title']) ? $data['title'] : 'Unknown';
+
 		//假如不要自动查找封面的话
-		if (isset($playerOptions['cover']))
-			if ($playerOptions['cover'] == 'false')
-				unset($playerOptions['cover']);
-
-		$options['music']['title'] = isset($playerOptions['title']) ? $playerOptions['title'] : '未知';
-		$options['music']['author'] = isset($playerOptions['artist']) ? $playerOptions['artist'] : '未知';
-		$options['music']['url'] = $source;
-		$options['music']['pic'] = isset($playerOptions['cover']) ? $playerOptions['cover'] : '';
+		if (isset($data['cover']))
+			if ($data['cover'] == 'false' || !boolval($data['cover']))
+				unset($data['cover']);
+			else 
+				$data['pic'] = $data['cover'];
 		
-		//输出代码
-		$playerCode =  '<div id="player'.$id.'" class="aplayer">
-		';
-		if ($playerOptions['lyric']) {
-			$playerCode .= '<pre class="aplayer-lrc-content">'."\n".$playerOptions['lyric']."\n</pre>\n";
-		}
-		$playerCode .= "</div>\n";
-		//加入头部数组
-		$js = json_encode($options);
-		$playerCode .= <<<EOF
-		<script>aPlayerOptions.push({$js});</script>
-EOF;
-		
-		return $playerCode;
-
+		return $data;
 	}
 	
 	/**
